1559adf735804762c03adbb3f0e1879afba4a082
Adekoya Dotun <adekoyateleola@gmail.com>
2023-03-27 05:28:07 -0700

    phython exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
         my_list_2 (list): The second list.
         list_length (int): The number of elements to divide.
     
-    Returns:
+     Returns:
         A new list of length list_length containing all the divisions.
     """
     new_list = []
